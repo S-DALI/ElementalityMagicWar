786db0e7a5f7da0cb5b0c2edb3b292a6bc72cf6d
ThuLiumNikx <138113914+ThuLiumNikx@users.noreply.github.com>
2023-07-07 23:30:32 +0100

    AddHealthSystem

---
+++ ElementalityMagicWar/Assets/Source/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int currentHealth;
+
+    private void Start()
+    {
+
+        if (maxHealth > 0)
+            currentHealth = maxHealth;
+    }
+
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+            Died();
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+    }
+    private void Died()
+    {
+        Destroy(gameObject);
+
+    }
+
+}
+
